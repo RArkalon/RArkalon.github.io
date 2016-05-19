42835e7b92f60918525ab9a6bd9a5e57d1cb315b
Robin DUDEK <rarkalon@hotmail.com>
2016-05-19 14:39:14 +0200

    ionic added

---
+++ ionic/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('newsCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+      
+.controller('detailCtrl', function($scope) {
+
+})
+   
+.controller('detail2Ctrl', function($scope) {
+
+})
+   
+.controller('detail3Ctrl', function($scope) {
+
+})
+ 
